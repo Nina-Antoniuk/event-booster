604c0560ac9e07a6f2185ea7aa807f7abf9b0f02
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-09-14 21:35:27 +0300

    Update buttonUp.js

--- src/js/buttonUp.js
+++ src/js/buttonUp.js
@@ -1,38 +1,29 @@
-const buttonUp = document.querySelector('#buttonScrollUp')
-const header = document.querySelector('.headerTemp')
-const sectionOne = document.querySelector('.section1Temp')
+const buttonUp = document.querySelector('#buttonScrollUp');
+const header = document.querySelector('.headerTemp');
+const sectionOne = document.querySelector('.section1Temp');
 const options = {
-    treshhold: 0.5,
-}
+  treshhold: 0.5,
+};
 
 function buttonUpActivator(entries, observer) {
-    entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-            buttonUp.classList.remove('isActive')
-            return
-        }
-        buttonUp.classList.add('isActive')
-    })
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      buttonUp.classList.remove('isActive');
+      return;
+    }
+    buttonUp.classList.add('isActive');
+  });
 }
 
-
-
-
-const observer = new IntersectionObserver(buttonUpActivator, options)
-
-
+const observer = new IntersectionObserver(buttonUpActivator, options);
 
 function scrollUp(e) {
-    header.scrollIntoView({
-        behavior: 'smooth',
-        block: 'end',
-    });
+  header.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
 
-observer.observe(header)
+observer.observe(header);
 
-
-
-
-buttonUp.addEventListener('click', scrollUp)
+buttonUp.addEventListener('click', scrollUp);
