e35c03f20c1623c545e7d9c880b52efd60609b58
Burkovskyi Dmytro <79474730+Amphy-nick@users.noreply.github.com>
2021-09-15 22:47:51 +0300

    зробив сітку

--- src/js/renderGalleryMarkup.js
+++ src/js/renderGalleryMarkup.js
@@ -12,19 +12,19 @@ export default function renderGalleryMarkup(events) {
         images,
       }) => {
         return `<li class ='gallery__item-card list'>
-      <div class='gallery__card'>
+     
         <div class='gallery__image'>
-          <img src='${images[0].url}' alt='${name}'class='img' width="180" />
+          <img src='${images[0].url}' alt='${name}'class='img gallery__img' width="180" />
         </div>
         <div class='gallery__meta'>
           <p class='gallery__meta_name'>${name}</p>
           <p class='gallery__meta_date'>${localDate}</p>
-            <svg class="contacts__icon--mail">
-              <use href="../images/svg/sprits.svg#icon-ticket"></use>
+            <svg class="icon">
+              <use href="./images/svg/sprits.svg#icon-ticket"></use>
             </svg>
           <p class='gallery__meta_place'>${venues[0].name}</p>
         </div>
-      </div>
+      
   </li>`;
       }
     )
