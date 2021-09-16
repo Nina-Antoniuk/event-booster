6d1c9d6aeff23bac9be0284f5eaff72751938a77
Tetiana Kalinichenko <79852838+Skorochka@users.noreply.github.com>
2021-09-16 23:54:08 +0300

    фиксит кнопки в футере + добавляет фото Нины

--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -1,36 +1,36 @@
-import refs from './refs.js'
+import refs from "./refs.js";
+import sprits from "../images/svg/sprits.svg";
 
-$(document).ready(function(){
-    const slider = $("#slider").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            768:{
-                items:3
-            },
-            
-        },
-        navText: [
-            '<svg class="arrow-left" width="20" height="20"><use class="icon" href="./images/svg/sprits.svg#icon-arrow-left"></use></svg>',
-            '<svg class="arrow-right" width="20" height="20"><use class="icon" href="./images/svg/sprits.svg#icon-arrow-right"></use></svg>'
-            ]
-    });
+$(document).ready(function () {
+  const slider = $("#slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 3,
+      },
+    },
+    navText: [
+      `<svg class="arrow-left" width="20" height="20"><use class="icon" href="${sprits}#icon-arrow-left"></use></svg>`,
+      `<svg class="arrow-right" width="20" height="20"><use class="icon" href="${sprits}#icon-arrow-right"></use></svg>`,
+    ],
+  });
 });
 
-refs.footerSection.addEventListener('click', modalFooterOpen)
-refs.footerModal.addEventListener('click', modalFooterClose)
+refs.footerSection.addEventListener("click", modalFooterOpen);
+refs.footerModal.addEventListener("click", modalFooterClose);
 
-function modalFooterOpen(){
-    refs.footerModal.classList.toggle('is-open')
+function modalFooterOpen() {
+  refs.footerModal.classList.toggle("is-open");
 }
 
-function modalFooterClose(e){
-    if( e.target === e.currentTarget){
-        refs.footerModal.classList.toggle('is-open')
-    }
-    return
+function modalFooterClose(e) {
+  if (e.target === e.currentTarget) {
+    refs.footerModal.classList.toggle("is-open");
+  }
+  return;
 }
