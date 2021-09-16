aaa5fb8ba725c66de2a17f0e48851866ca4593b7
Tetiana Kalinichenko <79852838+Skorochka@users.noreply.github.com>
2021-09-17 00:07:15 +0300

    Подключает notify для авторизац

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,72 +1,63 @@
-import refs from './refs.js'
-import {logInFetchLink, signInFetchLink} from './linksForFatch.js'
-import authWithEmailAndPassword from './fetchAuth.js'
+import refs from "./refs.js";
+import { logInFetchLink, signInFetchLink } from "./linksForFatch.js";
+import authWithEmailAndPassword from "./fetchAuth.js";
 
-let email = ''
-let password = ''
+let email = "";
+let password = "";
 
-refs.authBtn.addEventListener('click', onLoginButtonClick)
-refs.signInBtn.addEventListener('click', onSignInBtnClick)
-refs.formAuth.addEventListener('submit', authFormSubmit)
-refs.formSignIn.addEventListener('submit', signInFormSubmit)
-refs.formAuth.addEventListener('click', authFormClose)
-refs.formSignIn.addEventListener('click', signInFormClose)
-
-
+refs.authBtn.addEventListener("click", onLoginButtonClick);
+refs.signInBtn.addEventListener("click", onSignInBtnClick);
+refs.formAuth.addEventListener("submit", authFormSubmit);
+refs.formSignIn.addEventListener("submit", signInFormSubmit);
+refs.formAuth.addEventListener("click", authFormClose);
+refs.formSignIn.addEventListener("click", signInFormClose);
 
 function onLoginButtonClick(e) {
-    refs.formAuth.classList.toggle('is-open')
-
+  refs.formAuth.classList.toggle("is-open");
 }
-function onSignInBtnClick(e){
-    refs.formSignIn.classList.toggle('is-open')
+function onSignInBtnClick(e) {
+  refs.formSignIn.classList.toggle("is-open");
 }
 
 function authFormSubmit(e) {
-    e.preventDefault();
-    getEmailAndPassword(e)
-    authWithEmailAndPassword(email, password, logInFetchLink)
+  e.preventDefault();
+  getEmailAndPassword(e);
+  authWithEmailAndPassword(email, password, logInFetchLink);
 
-    clearEmailAndPassword(e)
-    refs.formAuth.classList.toggle('is-open')
-    
-
+  clearEmailAndPassword(e);
+  refs.formAuth.classList.toggle("is-open");
 }
 
-function signInFormSubmit(e){
-    e.preventDefault();
-    getEmailAndPassword(e)
-    authWithEmailAndPassword(email, password, signInFetchLink)
-    clearEmailAndPassword(e)
-    refs.formSignIn.classList.toggle('is-open')
+function signInFormSubmit(e) {
+  e.preventDefault();
+  getEmailAndPassword(e);
+  authWithEmailAndPassword(email, password, signInFetchLink);
+  clearEmailAndPassword(e);
+  refs.formSignIn.classList.toggle("is-open");
 }
 
-function getEmailAndPassword (e){
-    email = e.target.querySelector('.email').value
-    password = e.target.querySelector('.password').value
+function getEmailAndPassword(e) {
+  email = e.target.querySelector(".email").value;
+  password = e.target.querySelector(".password").value;
 }
 
-function clearEmailAndPassword(e){
-    email = ''
-    password = ''
-     e.target.querySelector('.email').value = ''
-   e.target.querySelector('.password').value = ''
+function clearEmailAndPassword(e) {
+  email = "";
+  password = "";
+  e.target.querySelector(".email").value = "";
+  e.target.querySelector(".password").value = "";
 }
 
-function authFormClose(e){
-    if( e.target === e.currentTarget){
-        refs.formAuth.classList.toggle('is-open')
-    }
-    return
+function authFormClose(e) {
+  if (e.target === e.currentTarget) {
+    refs.formAuth.classList.toggle("is-open");
+  }
+  return;
 }
 
-function signInFormClose(e){
-    if( e.target === e.currentTarget){
-        refs.formSignIn.classList.toggle('is-open')
-    }
-    return
+function signInFormClose(e) {
+  if (e.target === e.currentTarget) {
+    refs.formSignIn.classList.toggle("is-open");
+  }
+  return;
 }
-
-
-
-
--- src/js/fetchAuth.js
+++ src/js/fetchAuth.js
@@ -1,24 +1,41 @@
-import {resolve, reject} from './responseOnStatus'
+import { resolve, reject } from "./responseOnStatus";
+import Notify from "simple-notify";
+import "simple-notify/dist/simple-notify.min.css";
 
-
-function authWithEmailAndPassword (email, password, link) {
-
-    return fetch(`${link}`, {
-        method: 'POST',
-        body: JSON.stringify({
-            email, password,
-            returnSecureToken: true
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(r => {
-        if(r.status === 200){
-            resolve()
-        } 
-         return reject()
-        
-    }).catch(err => console.log(err))
+function authWithEmailAndPassword(email, password, link) {
+  return fetch(`${link}`, {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+      returnSecureToken: true,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((r) => {
+      if (r.status === 200) {
+        resolve();
+        new Notify({
+          status: "success",
+          title: "Authorization completed",
+          text: "Authorization completed",
+          effect: "fade",
+          type: 1,
+        });
+      } else {
+        reject();
+        new Notify({
+          status: "error",
+          title: "Try again",
+          text: "Try again",
+          effect: "fade",
+          type: 1,
+        });
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
-export default authWithEmailAndPassword
+export default authWithEmailAndPassword;
