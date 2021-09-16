062fc55788290b7762757a82faba0d7ff09c9169
Burkovskyi Dmytro <79474730+Amphy-nick@users.noreply.github.com>
2021-09-16 22:52:54 +0300

    Update

--- src/js/rendergallery.js
+++ src/js/rendergallery.js
@@ -33,32 +33,3 @@ export default function renderGalleryMarkup(events) {
     .join("");
   refs.galleryList.innerHTML = markup;
 }
-
-// import refs from "./refs";
-
-// export default function renderGalleryMarkup(events) {
-//   const markup = events
-//     .map(
-//       ({
-//         name,
-//         dates: {
-//           start: { localDate },
-//         },
-//         _embedded: { venues },
-//         images,
-//       }) => {
-//         return `<li class ='gallery__item-card list '>
-//     <div class='gallery__content'>
-//         <img src='${images[0].url}' alt='${name}'class='img' />
-//     </div>
-//     <div class='gallery__meta'>
-//       <p class='gallery__meta_name'>${name}</p>
-//       <p class='gallery__meta_date'>${localDate}</p>
-//       <p class='gallery__meta_place'>${venues[0].name}</p>
-//     </div>
-//   </li>`;
-//       }
-//     )
-//     .join("");
-//   refs.galleryList.innerHTML = markup;
-// }
