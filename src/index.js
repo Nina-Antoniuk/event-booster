70ee2ea63a6988b805e1454f3807f0f9c1ee266d
Nina Antoniuk <Antoniuk_Nina@ukr.net>
2021-09-19 16:23:15 +0300

    додала оутлайн на інпути

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "simple-notify/dist/simple-notify.min.css";
 import "./sass/main.scss";
-import "./js/get-countries";
-import "./js/event-request";
-import "./js/auth.js";
-import "./js/footer-modal.js";
 import fetchApi from "./js/fetchapi";
 import renderGalleryMarkup from "./js/rendergallery";
-import Pagination from "./js/pagination";
+import "./js/get-countries";
+import "./js/auth.js";
+import "./js/event-request";
+import "./js/modal";
+// import Pagination from "./js/pagination";
 import { pager } from "./js/pagination";
 import sprits from "./images/svg/sprits.svg";
-import "./js/modal";
+import "./js/footer-modal.js";
 
 window.addEventListener("DOMContentLoaded", onPageLoad);
 
